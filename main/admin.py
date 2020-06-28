bc637417819fab15229550c848500bd838cc787b
PeD1y <51740661+PeD1y@users.noreply.github.com>
2020-06-29 08:04:49 +0900

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from .models import User,Category,Book
 from django.contrib.auth.admin import UserAdmin
 
-
-
 admin.site.register(User, UserAdmin)
 admin.site.register(Category)
 admin.site.register(Book)
